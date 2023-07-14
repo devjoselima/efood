5d908372b6e026ff58d669e6e33d9161f6ceebc2
Jose Lima <jalimag0206@gmail.com>
2023-07-14 14:11:16 -0300

    fix: fix bugs on responsive layout

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,15 +1,24 @@
 import styled from 'styled-components'
+import { breakpoints } from '../../styles'
 
 export const FundoHeader = styled.div`
   width: 100%;
   height: 384px;
   display: block;
+
+  @media (max-width: ${breakpoints.tablet}) {
+    height: 320px;
+  }
 `
 
 export const Logo = styled.img`
   margin: 0 auto;
   display: block;
   padding: 64px 0 138.5px;
+
+  @media (max-width: ${breakpoints.tablet}) {
+    padding: 64px 0 50px;
+  }
 `
 
 export const Title = styled.h1`
@@ -19,4 +28,10 @@ export const Title = styled.h1`
   line-height: 42px;
   text-align: center;
   margin: 0 auto;
+
+  @media (max-width: ${breakpoints.tablet}) {
+    font-size: 28px;
+    width: 350px;
+    margin-bottom: 32px;
+  }
 `
--- src/components/RestaurantsList/styles.ts
+++ src/components/RestaurantsList/styles.ts
@@ -1,8 +1,15 @@
 import styled from 'styled-components'
+import { breakpoints } from '../../styles'
 
 export const ContainerProducts = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
   column-gap: 80px;
   margin-bottom: 120px;
+
+  @media (max-width: ${breakpoints.tablet}) {
+    grid-template-columns: 1fr;
+    justify-items: center;
+    max-width: 95%;
+  }
 `
